fa9d88a2ed53ebee24b4c386f57a9755fd5fe9b3
Arruda <arruda@uniriotec.com>
2011-03-14 11:33:39 -0300

    Changing the plotTag html and .py to use more options.
    Still missing a correction in plot.py in the method that prints the data.

--- PlotMaker/templatetags/plot_maker.py
+++ PlotMaker/templatetags/plot_maker.py
@@ -5,15 +5,25 @@ register = template.Library()
 
 
 @register.inclusion_tag('PlotMaker/tags/plotMakerTag.html')
-def plotMaker(plotData,placeholder="placeholder",width="600",height="300"):    
+def plotMaker(plotData,placeholder="placeholder",width="600",height="300",xAxisLabel=None,xAxisFontSize=None,xAxisFontFamily=None,xAxisCentralize=None,yAxisLabel=None,yAxisFontSize=None,yAxisFontFamily=None,yAxisCentralize=None):    
         
     
     try:
         dataString = printMultiplesPlotDatas(plotData)            
     except TypeError: 
         dataString = printSinglePlotData(plotData)                          
-                
-    return {'datasets': dataString,'placeholder':placeholder,'width':width,'height':height,'plotData':plotData}
+       
+    return {'datasets': dataString,'placeholder':placeholder,'width':width,'height':height,
+            'xAxisLabel':xAxisLabel,
+            'xAxisFontSize':xAxisFontSize,
+            'xAxisFontFamily':xAxisFontFamily,
+            'xAxisCentralize':xAxisCentralize,
+            
+            'yAxisLabel':yAxisLabel,
+            'yAxisFontSize':yAxisFontSize,
+            'yAxisFontFamily':yAxisFontFamily,
+            'yAxisCentralize':yAxisCentralize,            
+            }
 
 
 @register.inclusion_tag('PlotMaker/tags/plotMakerTag.html')
